1a017edc10233979ce6a0b6cc6e3f58f55d31707
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-09-24 10:20:59 -0500

    First solution for sumPairs using Map and Set with only one iteration through given Array

--- src/sumPairs/index.js
+++ src/sumPairs/index.js
@@ -3,6 +3,18 @@
  * @param {Array} A an array of numbers
  * @param {Number} N an integer value
  */
-function sumPairs(A, N) {}
+function sumPairs(arr, num) {
+  const numbers = new Set();
+  const solution = new Map();
+
+  for (let element of arr) {
+    numbers.add(element);
+    let diff = num - element;
+    if (numbers.has(diff) && diff !== element)
+      solution.set(Math.min(diff, element), Math.max(element, diff));
+  }
+
+  return Array.from(solution);
+}
 
 module.exports = sumPairs;
